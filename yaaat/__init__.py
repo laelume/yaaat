d2c10172c8d8abc963fc09b73bd8317afb9e7688
ashlae blum'e <laelume@gmail.com>
2025-11-15 11:46:55 +1300

    Create __init__.py

---
+++ yaaat/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2025 Ashlae Blum'e | laelume
+# Licensed under the MIT License
+
+"""YAAAT - Yet Another Audio Annotation Tool"""
+from .annotator import ChangepointAnnotator, main
+
+__version__ = "0.1.0"
+__author__ = "laelume"
+__license__ = "MIT"
+__all__ = ["ChangepointAnnotator", "main"]
