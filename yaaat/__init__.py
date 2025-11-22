4e1f03d899528f5d8edecdb7e2551ba279f603d2
ashlae blum'e <laelume@gmail.com>
2025-11-23 07:34:09 +1300

    Update __init__.py

--- yaaat/__init__.py
+++ yaaat/__init__.py
@@ -2,9 +2,12 @@
 # Licensed under the MIT License
 
 """YAAAT - Yet Another Audio Annotation Tool"""
+
 from .changepoint_annotator import ChangepointAnnotator, main
+from .peak_annotator import PeakAnnotator
+from .main import YAATApp, main
 
 __version__ = "0.1.0"
 __author__ = "laelume"
 __license__ = "MIT"
-__all__ = ["ChangepointAnnotator", "main"]
+__all__ = ["ChangepointAnnotator", "PeakAnnotator", "YAATApp", "main"]
